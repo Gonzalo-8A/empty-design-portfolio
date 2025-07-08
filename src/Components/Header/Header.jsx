35f5e5f120bceeae96aec48d1a7479a3254701ff
Gonzalo <gonzalo8amartin@gmail.com>
2025-07-08 12:56:27 +0200

    typo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { scroller } from 'react-scroll'
 import logo from '../../assets/logo.png'
-import Menu from '../Menu/Menu-2.js'
+import Menu from '../Menu/Menu-2.jsx'
 import './Header.css'
 import { useLocation, useNavigate } from 'react-router-dom'
 
