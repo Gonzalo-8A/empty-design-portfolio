5646f10214e9f0e177b86851ce480cf27c701951
Gonzalo <gonzalo8amartin@gmail.com>
2025-07-08 11:33:07 +0200

    Update dvh

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,26 @@ import './App.css'
 import { HashRouter } from 'react-router-dom'
 import AnimatedRoutes from './routes/AnimatedRoutes.jsx'
 import { PageProvider } from './context/page.jsx'
+import { useEffect } from 'react'
 
 function App () {
+  useEffect(() => {
+    const updateVh = () => {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+
+    updateVh()
+
+    window.addEventListener('resize', updateVh)
+    window.addEventListener('orientationchange', updateVh)
+
+    return () => {
+      window.removeEventListener('resize', updateVh)
+      window.removeEventListener('orientationchange', updateVh)
+    }
+  }, [])
+
   return (
     <HashRouter>
       <PageProvider>
